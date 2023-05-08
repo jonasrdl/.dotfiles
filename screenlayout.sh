d9947ef8cb8bb9335863c831a936fe2f61aed3f8
Jonas Riedel <jonasriedel@protonmail.com>
2023-05-08 20:16:42 +0200

    Create screenlayout.sh

---
+++ screenlayout.sh
@@ -0,0 +1,1 @@
+xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 0x346 --rotate normal --output DP-1 --off --output DP-2 --rate 144 --primary --mode 3440x1440 --reate 144.00 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off

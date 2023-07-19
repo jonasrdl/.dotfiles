b95ba383ecbcc83aa6109bc609d21163b4d9c9f0
jonas <jonasriedel@pm.me>
2023-07-19 11:37:00 +0200

    Create change-wallpaper.sh

---
+++ scripts/change-wallpaper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+WALLPAPER_DIR="/home/jriedel/Pictures/Wallpapers"
+
+change_wallpaper() {
+    wallpaper=$(find "$WALLPAPER_DIR" -type f \( -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" -o -name "*.gif" \) | shuf -n 1)
+    feh --bg-fill "$wallpaper"
+}
+
+change_wallpaper
